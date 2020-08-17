55afe661ab7da31e21c66dad82688c18be22663d
Shreeya <shreeya27naik@gmail.com>
2020-08-17 14:18:55 +0530

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,19 +9,19 @@ function App() {
   return (
     <div>
       <h1><span>emojipedia</span></h1>
-      
-      <dl className="dictionary">
-       {emojipedia.map(emoji =>(
-    <Entry
-      key={emoji.id}
-      emoji={emoji.emoji}
-      name={emoji.name}
-      meaning={emoji.meaning}
 
-    />
-  )
-       )}
-    </dl>
+      <dl className="dictionary">
+        {emojipedia.map(emoji => (
+          <Entry
+            key={emoji.id}
+            emoji={emoji.emoji}
+            name={emoji.name}
+            meaning={emoji.meaning}
+
+          />
+        )
+        )} 
+      </dl>
     </div>
   );
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import emojipedia from "../emojipedia";
 
 
-function Entry(props){ 
-return (
-<div className="term">
-<dt>
-<span className="emoji" role="img" aria-label="Tense Biceps">
-  {props.emoji}
-</span>
-<span>{props.name}</span>
-</dt>
-<dd>
-{props.meaning}</dd>
-</div>
-);
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>
+        {props.meaning}</dd>
+    </div>
+  );
 }
 
 
